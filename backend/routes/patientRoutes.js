b113a36e36438ec79ee34316139c51018b64e87b
vincebalaman <balaman.vincejaliel@gmail.com>
2026-04-06 08:12:57 +0800

    added schema

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -4,7 +4,19 @@ import cors from "cors";
 const router = express.Router();
 
 router.get("/patientlist", (req, res) => {
-    res.json({message: "List of patients"});
+    res.json({message: "Get list of patients"});
+});
+
+router.post("/patientlist", (req, res) => {
+    res.json({message: "Post list of patients"});
+});
+
+router.put("/patientlist", (req, res) => {
+    res.json({message: "Put list of patients"});
+});
+
+router.delete("/patientlist", (req, res) => {
+    res.json({message: "Delete list of patients"});
 });
 
 export default router;
